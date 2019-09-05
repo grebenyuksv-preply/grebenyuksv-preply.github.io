7c0ca36311efcdfca61e5952259a5f84bf690126
Sergii Grebeniuk <sergii.g@preply.com>
2019-09-05 17:14:03 +0300

    Initial

---
+++ dist/chat.6dda8d3194214267c936.js
@@ -0,0 +1,14 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["chat"],{
+
+/***/ "./src/dynamic.js":
+/*!************************!*\
+  !*** ./src/dynamic.js ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("console.log(\"Hello World from dynamic\");\n\n//# sourceURL=webpack:///./src/dynamic.js?");
+
+/***/ })
+
+}]);
---
+++ src/pageOne.js
@@ -0,0 +1,6 @@
+console.log('Hello World from pageOne main file!');
+
+import(
+	/* webpackChunkName: "chat" */
+	`./dynamic.js`
+);
